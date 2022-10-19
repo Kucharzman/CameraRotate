754b75fe07c66dd154e300e571608c572fa63f2a
test <test@asd.pl>
2022-10-19 10:11:16 +0200

    2.1 Camera permissions

--- app/src/main/java/com/example/camerarotate/MainActivity.kt
+++ app/src/main/java/com/example/camerarotate/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.camerarotate
 
+import android.app.Activity
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.core.app.ActivityCompat
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.bttCamera).isEnabled = false
+
+        if (ActivityCompat.checkSelfPermission(this,android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA), 111)
+        }
+        else{
+            findViewById<Button>(R.id.bttCamera).isEnabled = true
+        }
+
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == 111 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+            findViewById<Button>(R.id.bttCamera).isEnabled = true
+        }
+
+    }
+
 }
